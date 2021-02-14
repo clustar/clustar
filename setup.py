03fd167d7257b141dc4cce1c1cdf02764ad1688f
jz5jx <42184372+jz5jx@users.noreply.github.com>
2021-02-13 19:45:53 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     keywords='example, pypi, package',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/tomchen/example_pypi_package',
+    url='/https://github.com/clustar/clustar',
     project_urls={
         'Documentation': 'NA',
         'Bug Reports':
