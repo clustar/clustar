b142cc880f582dbfc49fefd6c2ea2a0263b2d190
jz5jx <jz5jx@virginia.edu>
2021-02-13 19:29:35 -0500

    imports

--- src/examplepy/module1.py
+++ src/examplepy/module1.py
@@ -1,5 +1,18 @@
 # Example PyPI (Python Package Index) Package
-
+import unittest
+from astropy.io import fits
+import astropy.io
+import numpy as np
+import matplotlib
+from astropy.table import Table
+import astropy.io
+from scipy import stats
+from matplotlib import pyplot as plt
+from matplotlib.patches import Ellipse
+from astroML.stats import fit_bivariate_normal
+from astroML.stats.random import bivariate_normal
+import numpy as np
+import pandas as pd
 class test_num(object):
 
     def __init__(self, n):
