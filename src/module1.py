ba5032a92e0b2e6184ebbde97b08c34ed1ff8390
spaylor4 <spaylor4@yahoo.com>
2021-03-05 15:18:16 -0500

    Remove unused imports

--- src/module1.py
+++ src/module1.py
@@ -1,19 +1,16 @@
 # Example PyPI (Python Package Index) Package
 import unittest
-from astropy.io import fits
-import astropy.io
-import numpy as np
-import matplotlib
-from astropy.table import Table
-import astropy.io
-from scipy import stats
-from matplotlib import pyplot as plt
-from matplotlib.patches import Ellipse
-from astroML.stats import fit_bivariate_normal
-from astroML.stats.random import bivariate_normal
 import numpy as np
 import pandas as pd
+from scipy import stats
 import pkg_resources
+# import matplotlib
+# from matplotlib import pyplot as plt
+# from matplotlib.patches import Ellipse
+# from astropy.table import Table
+# import astropy.io
+# from astroML.stats import fit_bivariate_normal
+# from astroML.stats.random import bivariate_normal
 
 import clustarray
 
