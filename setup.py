7b956ac03911812624ae1f010aeaf8351676fbf7
jz5jx <42184372+jz5jx@users.noreply.github.com>
2021-03-07 15:39:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     keywords='cluster, astronomy, protostars',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/clustar/clustar/tree/main/src',
+    url='https://github.com/clustar/clustar',
     project_urls={
         'Documentation': 'NA',
         'Bug Reports':
