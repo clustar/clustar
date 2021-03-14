4f0295e5d21345954315071debc807b8f4431a71
pkbondalapati <pavan.bond.kumar@gmail.com>
2021-03-14 05:53:29 -0400

    Merged ClustarStats into ClustarData

--- src/core.py
+++ src/core.py
@@ -3,60 +3,64 @@ class ClustarData(object):
 
     class _Image(object):
 
-        def __init__(self, image):
+        def __init__(self):
+            self.data = None
+            self.bounds = None
             self.x = None
             self.y = None
-            self.z = image
             self.ref = None
             self.limit = None
             self.pos = None
-            self.bvg = None
 
-    class _Residual(object):
+    class _Residuals(object):
 
-        def __init__(self, residuals):
-            self.pos = residuals
+        def __init__(self):
+            self.pos = None
             self.inside = None
             self.outside = None
 
-    def __init__(self, image, residuals):
-        self.image = image
-        self.residuals = residuals
+    class _Fit(object):
 
+        def __init__(self):
+            self.bvg = None
+            self.ellipse = None
 
-class ClustarStats(object):
+    class _Stats(object):
 
-    def __init__(self, image, group_range):
-        self.image = image
-        self.group_range = group_range
+        def __init__(self):
+            self.x_bar = None
+            self.y_bar = None
+            self.x_var = None
+            self.y_var = None
+            self.cov = None
+            self.cov_mat = None
+            self.rho = None
+            self.eigen_values = None
+            self.eigen_vectors = None
 
-    def mean(self):
-        pass
+    class _Metrics(object):
 
-    def variance(self):
-        pass
+        def __init__(self):
+            self.standard_deviation = None
+            self.variance = None
+            self.average = None
+            self.weighted_average = None
 
-    def covariance(self):
-        pass
-
-    def covariance_matrix(self):
-        pass
-
-    def rho(self):
-        pass
-
-    def eigen_values(self):
-        pass
-
-    def eigen_vectors(self):
-        pass
-
-    def angle(self):
-        pass
-
-    def ellipse(self):
-        pass
+    def __init__(self):
+        self.image = self._Image()
+        self.residuals = self._Residuals()
+        self.fit = self._Fit()
+        self.stats = self._Stats()
+        self.metrics = self._Metrics()
 
 
 if __name__ == "__main__":
-    print("Hello")
+    import numpy as np
+    image = np.zeros((10, 10))
+    image[1:4, 1:4] = 1
+    image[1:4, 6:9] = 1
+    image[6:9, 1:4] = 1
+    image[6:9, 6:9] = 1
+    cd = ClustarData()
+    cd.image.data = image
+    print(cd.image.data)
