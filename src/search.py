c5535c6e1ad693bdc19d89919c377fb935533810
pkbondalapati <pavan.bond.kumar@gmail.com>
2021-03-17 11:49:58 -0400

    Updated Clustar object to integrate ClustarData object

--- src/search.py
+++ src/search.py
@@ -1,29 +1,17 @@
 from astropy.io import fits
 import numpy as np
 import sys
-import group
-import fit
-
-import clustarray
+from core import ClustarData
 
 
 class Clustar(object):
 
-    def __init__(self, buffer_size=15, group_size=100, subgroup_factor=0.5,
-                 group_factor=0.0, metric="variance", threshold=0.01):
-        self.buffer_size = buffer_size
-        self.group_size = group_size
-        self.subgroup_factor = subgroup_factor
-        self.group_factor = group_factor
-        self.metric = metric
-        self.threshold = threshold
-        self.group_ranges = []
-        self.group_data = []
-        self.group_stats = []
+    def __init__(self, **kwargs):
+        self.params = kwargs
 
     def run(self, files):
-        select = []
         output = []
+        errors = []
         for i, file_path in enumerate(files):
             file = fits.open(file_path)
             origin_image = file[0].data[0, 0, :, :]
@@ -32,34 +20,17 @@ class Clustar(object):
             if image.shape[0] > 2000:
                 continue
 
-            # -- Denoise Here --
-            # std = np.std(image)
-            # image[image < std * 5] = 0
-
-            clust = clustarray.ClustArray(image)
-            clust.denoise()
-            image = clust.denoised_arr
-            image[image < clust.noise_est * 5] = 0
-            # -- ------------ --
-
             try:
-                self.group_ranges, self.group_data, self.group_stats = \
-                    group.arrange(image, self.buffer_size, self.group_size,
-                                  self.subgroup_factor, self.group_factor)
-
-                self.group_data, self.group_stats, output = \
-                    fit.bivariate_gaussian(origin_image, self.metric,
-                                           self.threshold, self.group_ranges,
-                                           self.group_data, self.group_stats)
+                cd = ClustarData(image, **self.params)
+                if cd.flag:
+                    output.append(cd)
 
             except Exception:
-                select.append(file_path)
-
-            if True in output:
-                select.append(file_path)
+                errors.append(file_path)
 
             sys.stderr.write(f'\rFile: {i + 1}/{len(files)} ' +
-                             f'| Flagged: {len(select)}')
+                             f'| Flagged: {len(output)} ' +
+                             f'| Errors: {len(errors)}')
             sys.stderr.flush()
 
-        return select
+        return output, errors
